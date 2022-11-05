fa5f09ffb237228b4011b174a795f197700be910
TscherterJunior <74146580+TscherterJunior@users.noreply.github.com>
2022-11-05 11:01:46 +0100

    api coments
    05.11

--- api.php
+++ api.php
@@ -4,22 +4,20 @@ header("Content-Type: application/json");
 $method = $_SERVER['REQUEST_METHOD'];
 
 if($method == 'GET') {
-    if (isset($_GET['id'])) {
+    if (isset($_GET['id'])) { // Testet ob die Anfrage Spezifisch ist oder nicht
         $id = $_GET['id'];
-        $id = preg_replace('/[^a-z0-9]/', '', $id );
-        //$raw = file_get_contents("./trails/$id.json");
-        //$obj = json_decode($raw, true);
-        //echo json_encode($obj, JSON_PRETTY_PRINT);
-        echo file_get_contents("./trails/$id.json");
+        $id = preg_replace('/[^a-z0-9]/', '', $id ); // Sanitize id
+        echo file_get_contents("./trails/$id.json"); // JSON datei zurück schicken
     } else {
         $list = [];
-        $ids = scandir("./trails/");
+        $ids = scandir("./trails/"); //trails ordner scannen
         foreach($ids as $id) {
             if ($id[0]=='.') continue;
             $id = explode('.', $id);
             $id = $id[0];
             $list[] = $id;
-        }
-        echo json_encode($list);
+            // liste komposieren
+        } 
+        echo json_encode($list); // liste zurück schicken
     }
 }
