a29baaa66318ba7520f0789b1b8d8a5cfb3ba793
TscherterJunior <74146580+TscherterJunior@users.noreply.github.com>
2022-11-05 11:15:42 +0100

    serviceworker comments

--- service-worker.js
+++ service-worker.js
@@ -3,50 +3,43 @@ const assets = [
   "/",
   "/index.html",
   "/main.js",
-  //"/vendor/",
   "/vendor/bss.js",
   "/vendor/mergerino.js",
   "/vendor/mithril.js",
   "/vendor/qr-scanner-worker.min.js",
   "/vendor/qr-scanner.min.js",
   "/icon.png",
-  //"http://localhost/api.php?get",
   "/favicon.ico"
- // "http://localhost/api.php/get?id=lorem"
 ]
 
-self.addEventListener("install", installEvent => {
+self.addEventListener("install", installEvent => { // on install event
   self.skipWaiting()
   installEvent.waitUntil(
-    caches.open(cacheadress).then(cache => {
-      cache.addAll(assets).catch((e) => {
-		  console.log(e)
-	  })
+    caches.open(cacheadress).then(cache => {      // cache öffnen 
+      cache.addAll(assets)  // in assets gelistete dateien cachen
     })
   )
 })
 
-  self.addEventListener("fetch", fetchEvent => {
+  self.addEventListener("fetch", fetchEvent => { // interupt fetch
     fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-        //console.log(fetchEvent.request,"t")
+      caches.match(fetchEvent.request).then(res => { // wenn die angeforderte datei gecacht ist mit der gecachten version antworten
         return res || fetch(fetchEvent.request)
       })
     )
   })
 
   self.addEventListener("activate", (event) => {
-    event.waitUntil(clients.claim());
+    event.waitUntil(clients.claim());  // service worker in aktiven zustand versetzen 
   });
 
-  self.addEventListener("message",(m) => {
-    //console.log(m.data)
-    if (m.data.type == "doinstal"){
+  self.addEventListener("message",(m) => { // messages werden verwendet um dem service worker zu sagen welcher trail installiert oder denistalliert werden soll
+    if (m.data.type == "doinstal"){ // install
       caches.open(cacheadress).then(cache => {
         cache.addAll([`https://ompha.li/api.php?id=${m.data.trail}`])
       })
     }
-    else if(m.data.type == "dodelete"){
+    else if(m.data.type == "dodelete"){ // deinstall
       caches.open(cacheadress).then(cache => {
         cache.delete(`https://ompha.li/api.php?id=${m.data.trail}`)
       })
